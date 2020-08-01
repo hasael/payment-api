34bfd8f150dc87bbe39151808607518f4f4a7042
hasael <elmanhasa@gmail.com>
2020-08-01 15:07:53 +0200

    Add lambda support with serverless

--- build.sbt
+++ build.sbt
@@ -1,13 +1,23 @@
-name := "payment-api"
+
+name := "payment-api-lambda"
 
 version := "0.1"
+scalaVersion := "2.12.8"
+scalacOptions += "-Ypartial-unification"
+assemblyJarName in assembly := "payment-api-lambda.jar"
 
-scalaVersion := "2.13.1"
 val http4sVersion = "0.21.5"
 val catsEffectVersion = "2.1.3"
+val circeVersion = "0.12.3"
+
 libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-dsl" % http4sVersion,
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
   "org.http4s" %% "http4s-blaze-client" % http4sVersion,
-  "org.typelevel" %% "cats-effect" % catsEffectVersion
+  "org.http4s" %% "http4s-circe" % http4sVersion,
+  "io.circe" %% "circe-core" % circeVersion,
+  "io.circe" %% "circe-generic" % circeVersion,
+  "io.circe" %% "circe-parser" % circeVersion
 )
+
+addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.0-M1")
--- src/main/scala/Launch.scala
+++ src/main/scala/Launch.scala
@@ -1,18 +1,27 @@
+
+import Launch.{paymentService, routes}
 import cats.effect.{ExitCode, IO, IOApp}
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
-import routes.{PaymentRoutes, VersionRoutes}
+import _root_.routes.{PaymentRoutes, VersionRoutes}
 import services.PaymentService
 import cats.implicits._
+import org.http4s.HttpApp
+import org.http4s._
+import cats.effect._
+import org.http4s.server.middleware._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object Launch extends IOApp {
-
+trait Routes {
   val paymentService = PaymentService.impl[IO]
 
   val routes = PaymentRoutes(paymentService) <+>
     VersionRoutes()
+}
+
+object Launch extends IOApp with Routes {
+
 
   override def run(args: List[String]): IO[ExitCode] = {
 
@@ -23,5 +32,12 @@ object Launch extends IOApp {
       .compile
       .drain
       .as(ExitCode.Success)
+  }
+}
+
+class LambdaEntryPoint extends LambdaHandler with Routes {
+
+  def run: IO[HttpApp[IO]] = IO {
+    routes.orNotFound
   }
 }
